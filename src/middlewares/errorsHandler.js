39fdfde9bdabe5c10763800e282b058d56bf071b
Mariano Silva <mariano.silva@ufvjm.edu.br>
2024-01-22 15:22:40 -0300

    feat: logging middleware error

--- src/middlewares/errorsHandler.js
+++ src/middlewares/errorsHandler.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 // eslint-disable-next-line no-unused-vars
 function errorsHandler(error, req, res, next){
 
+  console.error(error);
+
   if(error instanceof mongoose.Error.CastError){
     return res.status(400).json({message: "Um ou mais dados fornecidos estão incorretos"});
   }
